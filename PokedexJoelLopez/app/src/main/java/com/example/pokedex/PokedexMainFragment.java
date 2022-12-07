3c0c8a58ad196a99e7dcd3e051fdf82503805a95
joellpz <joelme001@gmail.com>
2022-12-07 14:43:23 +0100

    intro y bienvenido funcionan NO TOCAR. Tabbed Funciona falta formatear

--- PokedexJoelLopez/app/src/main/java/com/example/pokedex/PokedexMainFragment.java
+++ PokedexJoelLopez/app/src/main/java/com/example/pokedex/PokedexMainFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import com.example.pokedex.databinding.FragmentPokedexMainBinding;
@@ -15,7 +17,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 public class PokedexMainFragment extends Fragment {
             private FragmentPokedexMainBinding binding;
-
+            private NavController navController;
             @Override
             public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
                 return (binding = FragmentPokedexMainBinding.inflate(inflater, container, false)).getRoot();
@@ -24,6 +26,15 @@ public class PokedexMainFragment extends Fragment {
             @Override
             public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
                 super.onViewCreated(view, savedInstanceState);
+
+                navController = Navigation.findNavController(view);
+
+                binding.btnBack.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        navController.navigate(R.id.action_pokedexMainFragment_to_nav_graph);
+                    }
+                });
 
                 binding.viewPager.setAdapter(new FragmentStateAdapter(this) {
                     @NonNull
